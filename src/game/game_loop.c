1575b6d9537641d8110364923a0e138645c3cfe7
SimoneFelici <felicisimone9@gmail.com>
2025-08-19 21:12:35 +0200

    movement

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -71,9 +71,42 @@ void render_test_screen(t_game *game)
 	mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.img, 0, 0);
 }
 
+void	move_player(t_game *game, double dir_x, double dir_y)
+{
+	double	new_x;
+	double	new_y;
+	double	delta_x;
+	double	delta_y;
+	double	move_speed;
+
+	move_speed = 0.1;
+	delta_x = dir_x * move_speed;
+	delta_y = dir_y * move_speed;
+	new_x = game->player.pos_x + delta_x;
+	new_y = game->player.pos_y + delta_y;
+	if (game->map.grid[(int)new_y][(int)game->player.pos_x] == '0')
+		game->player.pos_y = new_y;
+	if (game->map.grid[(int)game->player.pos_y][(int)new_x] == '0')
+		game->player.pos_x = new_x;
+}
+
+void    rot_player(t_game *game, double direction)
+{
+	double  rot_speed;
+	double  old_dir_x;
+	double  old_plane_x;
+
+	rot_speed = 0.1 * direction;
+	old_dir_x = game->player.dir_x;
+	old_plane_x = game->player.plane_x;
+	game->player.dir_x = game->player.dir_x * cos(rot_speed) - game->player.dir_y * sin(rot_speed);
+	game->player.dir_y = old_dir_x * sin(rot_speed) + game->player.dir_y * cos(rot_speed);
+	game->player.plane_x = game->player.plane_x * cos(rot_speed) - game->player.plane_y * sin(rot_speed);
+	game->player.plane_y = old_plane_x * sin(rot_speed) + game->player.plane_y * cos(rot_speed);
+}
+
 int key_press(int keycode, t_game *game)
 {
-	
 	if (keycode == 65307) // ESC su macOS (era 65307 che è Linux)
 	{
 		printf("ESC pressed, closing game...\n");
@@ -84,19 +117,33 @@ int key_press(int keycode, t_game *game)
 	else if (keycode == 119) // W
 	{
 		printf("W pressed\n");
+		move_player(game, game->player.dir_x, game->player.dir_y);
 	}
 	else if (keycode == 115) // S
 	{
 		printf("S pressed\n");
+		move_player(game, -game->player.dir_x, -game->player.dir_y);
 	}
 	else if (keycode == 97) // A
 	{
 		printf("A pressed\n");
+		move_player(game, -game->player.plane_x, -game->player.plane_y);
 	}
 	else if (keycode == 100) // D
 	{
 		printf("D pressed\n");
+		move_player(game, game->player.plane_x, game->player.plane_y);
 	}
+	else if (keycode == 65361) // Freccia sinistra
+    {
+        printf("Left arrow pressed\n");
+        rot_player(game, -1.0);
+    }
+    else if (keycode == 65363) // Freccia destra
+    {
+        printf("Right arrow pressed\n");
+        rot_player(game, 1.0);
+    }
 
 /*	APPLE
 	else if (keycode == 13) // W
